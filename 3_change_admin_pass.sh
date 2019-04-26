f689874f4b4093f32f8f05ceb9056bd859629bbb
QuincyChengAtWork <quincy.cheng@cyberark.com>
2019-04-26 09:09:29 +0800

    Update 3_change_admin_pass.sh

--- 3_change_admin_pass.sh
+++ 3_change_admin_pass.sh
@@ -6,10 +6,10 @@ set -euo pipefail
 export API_KEY_ADMIN=$(cat admin.key)
 
 
-docker run --rm -it -v $(PWD)/mydata/:/root --entrypoint bash cyberark/conjur-cli:5 -c "yes yes | conjur init -a $CONJUR_ACCOUNT -u $CONJUR_URL"
-docker run --rm -it -v $(PWD)/mydata/:/root cyberark/conjur-cli:5 authn login -u admin -p $API_KEY_ADMIN
-docker run --rm -it -v $(PWD)/mydata/:/root cyberark/conjur-cli:5 user update_password -p $CONJUR_ADMIN_PASSWORD
-docker run --rm -it -v $(PWD)/mydata/:/root cyberark/conjur-cli:5 authn logout
+docker run --rm -it --add-host conjur.demo.com:$(SERVICE_IP) -v $(pwd)/mydata/:/root --entrypoint bash cyberark/conjur-cli:5 -c "yes yes | conjur init -a $CONJUR_ACCOUNT -u $CONJUR_URL"
+docker run --rm -it --add-host conjur.demo.com:$(SERVICE_IP) -v $(pd)/mydata/:/root cyberark/conjur-cli:5 authn login -u admin -p $API_KEY_ADMIN
+docker run --rm -it --add-host conjur.demo.com:$(SERVICE_IP) -v $(pwd)/mydata/:/root cyberark/conjur-cli:5 user update_password -p $CONJUR_ADMIN_PASSWORD
+docker run --rm -it --add-host conjur.demo.com:$(SERVICE_IP) -v $(pwd)/mydata/:/root cyberark/conjur-cli:5 authn logout
 
 
  cp ./mydata/conjur-$CONJUR_ACCOUNT.pem .
