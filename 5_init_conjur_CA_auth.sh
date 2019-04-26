cd89b7e04d469855e42c76a014e734c406fba571
QuincyChengAtWork <quincy.cheng@cyberark.com>
2019-04-26 09:48:33 +0800

    Update 5_init_conjur_CA_auth.sh

--- 5_init_conjur_CA_auth.sh
+++ 5_init_conjur_CA_auth.sh
@@ -1,10 +1,12 @@
-
+export SERVICE_IP=$(kubectl get svc --namespace conjur \
+                                          conjur-oss-ingress \
+                                          -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 
 ## inicializa CA y almacena llaves en conjur
 
 cp startCA.sh mydata/startCA.sh
 
-docker run --rm -it -v $(PWD)/mydata/:/root cyberark/conjur-cli:5 authn login -u admin -p $CONJUR_ADMIN_PASSWORD
-docker run --rm -it -v $(PWD)/mydata/:/root -e AUTHENTICATOR_ID -e CONJUR_ACCOUNT  \
+docker run --rm -it --add-host conjur.demo.com:$SERVICE_IP -v $(pwd)/mydata/:/root cyberark/conjur-cli:5 authn login -u admin -p $CONJUR_ADMIN_PASSWORD
+docker run --rm -it --add-host conjur.demo.com:$SERVICE_IP -v $(pwd)/mydata/:/root -e AUTHENTICATOR_ID -e CONJUR_ACCOUNT  \
         --entrypoint bash cyberark/conjur-cli:5 /root/startCA.sh
 
