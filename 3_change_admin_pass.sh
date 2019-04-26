94cba6a189d7265d29449ae858fc7a047f916e27
QuincyChengAtWork <quincy.cheng@cyberark.com>
2019-04-26 09:21:42 +0800

    Update 3_change_admin_pass.sh

--- 3_change_admin_pass.sh
+++ 3_change_admin_pass.sh
@@ -8,7 +8,7 @@ export SERVICE_IP=$(kubectl get svc --namespace conjur \
                                           -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 
 docker run --rm -it --add-host conjur.demo.com:$SERVICE_IP -v $(pwd)/mydata/:/root --entrypoint bash cyberark/conjur-cli:5 -c "yes yes | conjur init -a $CONJUR_ACCOUNT -u $CONJUR_URL"
-docker run --rm -it --add-host conjur.demo.com:$SERVICE_IP -v $(pd)/mydata/:/root cyberark/conjur-cli:5 authn login -u admin -p $API_KEY_ADMIN
+docker run --rm -it --add-host conjur.demo.com:$SERVICE_IP -v $(pwd)/mydata/:/root cyberark/conjur-cli:5 authn login -u admin -p $API_KEY_ADMIN
 docker run --rm -it --add-host conjur.demo.com:$SERVICE_IP -v $(pwd)/mydata/:/root cyberark/conjur-cli:5 user update_password -p $CONJUR_ADMIN_PASSWORD
 docker run --rm -it --add-host conjur.demo.com:$SERVICE_IP -v $(pwd)/mydata/:/root cyberark/conjur-cli:5 authn logout
 
