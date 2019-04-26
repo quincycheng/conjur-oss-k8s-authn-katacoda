cb2b20da63537dd8600777ea957710665657f467
QuincyChengAtWork <quincy.cheng@cyberark.com>
2019-04-26 09:14:59 +0800

    Update 3_change_admin_pass.sh

--- 3_change_admin_pass.sh
+++ 3_change_admin_pass.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 set -euo pipefail
 
-#cambia la contraseña  de admin por Cyberark1 a menos que se especifique algo diferente en bootstrap.env
-#
-export API_KEY_ADMIN=$(cat admin.key)
 
+export API_KEY_ADMIN=$(cat admin.key)
+export SERVICE_IP=$(kubectl get svc --namespace conjur \
+                                          conjur-oss-ingress \
+                                          -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 
 docker run --rm -it --add-host conjur.demo.com:$SERVICE_IP -v $(pwd)/mydata/:/root --entrypoint bash cyberark/conjur-cli:5 -c "yes yes | conjur init -a $CONJUR_ACCOUNT -u $CONJUR_URL"
 docker run --rm -it --add-host conjur.demo.com:$SERVICE_IP -v $(pd)/mydata/:/root cyberark/conjur-cli:5 authn login -u admin -p $API_KEY_ADMIN
 docker run --rm -it --add-host conjur.demo.com:$SERVICE_IP -v $(pwd)/mydata/:/root cyberark/conjur-cli:5 user update_password -p $CONJUR_ADMIN_PASSWORD
 docker run --rm -it --add-host conjur.demo.com:$SERVICE_IP -v $(pwd)/mydata/:/root cyberark/conjur-cli:5 authn logout
 
-
- cp ./mydata/conjur-$CONJUR_ACCOUNT.pem .
+cp ./mydata/conjur-$CONJUR_ACCOUNT.pem .
